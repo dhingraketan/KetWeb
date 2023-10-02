9648a03a3138e46e4e78a6cbc915a6b6c7572164
Ketan <kda59@sfu.ca>
2023-10-02 03:56:47 -0700

    scroll animation added on heading

--- src/app/introwave/introwave.component.ts
+++ src/app/introwave/introwave.component.ts
@@ -7,28 +7,7 @@ import { Component, HostListener } from '@angular/core';
 })
 export class IntrowaveComponent {
 
-  mainHeadingFontSize = 4;
-  mainHeadingOpacity = 1;
-  subHeadingVisible = true;
-  serviceTilesVisible = true;
 
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event) {
-    const scroll = window.scrollY || document.documentElement.scrollTop;
-
-    // Update mainheading font size and opacity
-    this.mainHeadingFontSize = 4 + scroll / 200;
-    this.mainHeadingOpacity = 1 - scroll / 500;
-
-    // Hide subheading and service-tiles on scroll
-    if (scroll > 200) {
-      this.subHeadingVisible = false;
-      this.serviceTilesVisible = false;
-    } else {
-      this.subHeadingVisible = true;
-      this.serviceTilesVisible = true;
-    }
-  }
 
 
 }
