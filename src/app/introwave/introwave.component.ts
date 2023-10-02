bf197bca4a4f2b935f4a8cfa72c8750196bf24a4
Ketan <kda59@sfu.ca>
2023-10-02 01:03:20 -0700

    layout management

--- src/app/introwave/introwave.component.ts
+++ src/app/introwave/introwave.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-introwave',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./introwave.component.css']
 })
 export class IntrowaveComponent {
+
+  mainHeadingFontSize = 4;
+  mainHeadingOpacity = 1;
+  subHeadingVisible = true;
+  serviceTilesVisible = true;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const scroll = window.scrollY || document.documentElement.scrollTop;
+
+    // Update mainheading font size and opacity
+    this.mainHeadingFontSize = 4 + scroll / 200;
+    this.mainHeadingOpacity = 1 - scroll / 500;
+
+    // Hide subheading and service-tiles on scroll
+    if (scroll > 200) {
+      this.subHeadingVisible = false;
+      this.serviceTilesVisible = false;
+    } else {
+      this.subHeadingVisible = true;
+      this.serviceTilesVisible = true;
+    }
+  }
+
 
 }
