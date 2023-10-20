248bd3bf4c5fe5c058aa91b8be8438fe18925ac9
Ketan <kda59@sfu.ca>
2023-10-20 01:47:17 -0700

    dynamic background added to the navbar. changes on sroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ketweb';
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', function () {
+      var targetElement = document.querySelector('.about');
+      var nav = document.querySelector('.navbr');
+      var elementRect = targetElement!.getBoundingClientRect();
+      var bodyRect = document.body.getBoundingClientRect();
+
+      // Check if the element's top position is within the viewport
+      if (elementRect.top >= + 90 && elementRect.top <= bodyRect.height) {
+        nav!.classList.remove('nav');
+      } else {
+        console.log('Element is not at the top of the page.');
+        nav!.classList.add('nav');
+      }
+    });
+  }
+
 }
